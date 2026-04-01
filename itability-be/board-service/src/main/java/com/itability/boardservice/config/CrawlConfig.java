d655a8c35224926450892ad59cf23ada861ae828
악덕 <gdpark.dev@gmail.com>
2026-04-01 09:58:44 +0900

    fix: board-service 미사용 EntityScan import 제거
    - CrawlConfig.java에서 사용하지 않는 import로 빌드 실패

--- itability-be/board-service/src/main/java/com/itability/boardservice/config/CrawlConfig.java
+++ itability-be/board-service/src/main/java/com/itability/boardservice/config/CrawlConfig.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
